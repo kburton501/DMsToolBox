c0089e593a546fa929fb07731ebffae9b45fd527
kburton501 <kburton501@hotmail.com>
2018-11-08 01:33:09 -0800

    add format Ability bonus display

--- DMsToolBox/PlayerRace.cpp
+++ DMsToolBox/PlayerRace.cpp
@@ -2,7 +2,7 @@
 #include "PlayerRace.h"
 #include <fstream>
 #include <iostream>
-
+#include <iomanip>
 
 
 
@@ -21,8 +21,10 @@ PlayerRace::~PlayerRace()
 	//output number of stuct created in vector
 	std::cout << std::endl;
 	std::cout << "Number of Elements: " << r.size() << std::endl;
+
 	//deletes all elements in vector
 	r.clear();
+
 	//output number of stuct created in vector after deletion
 	//should be 0
 	std::cout << "Number of Elements: " << r.size() << std::endl;
@@ -40,7 +42,9 @@ void PlayerRace::buildRaceStruct(std::vector<Race*>* r) {
 	{
 		//skips past first row
 		std::getline(RaceTable, line);
-
+		std::cout << "Race      Str Dex Con Int Wis Cha";
+		//create a struct for each race
+		//stores struct data in respective places
 		for (int i = 0; i < 9; i++) {
 			Race * obj = new Race;
 			(*r).push_back(obj);
@@ -50,37 +54,37 @@ void PlayerRace::buildRaceStruct(std::vector<Race*>* r) {
 			//Gets name stores name in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->name = line;
-			std::cout << line << " ";
+			std::cout << std::left << std::setw(10) << line << " ";
 
 			//Gets str stores str in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->strBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//Gets dex stores dex in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->dexBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//Gets con stores con in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->conBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//Gets intel stores intel in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->intelBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//Gets wis stores wis in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->wisBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//Gets cha stores cha in struct and displays
 			std::getline(RaceTable, line, ',');
 			obj->charisBonus = stoi(line);
-			std::cout << line << " ";
+			std::cout << std::setw(3) << line << " ";
 
 			//skips past currently unused info
 			std::getline(RaceTable, line);
